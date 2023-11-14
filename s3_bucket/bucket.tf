6249bcaea158db5a920de09ec8db1ea8bdeaa606
nayeem <snayeem753@gmail.com>
2023-11-14 15:26:33 +0530

    updaate a new file

---
+++ s3_bucket/bucket.tf
@@ -0,0 +1,15 @@
+# create s3 bucket
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "mybucketeks10" # change this
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "dynamo_db_table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
