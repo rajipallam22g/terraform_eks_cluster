904d6d19c0b8b6fdb56f84c7ffbc358b8dbd4629
Shaiknayeem753 <108930019+Shaiknayeem753@users.noreply.github.com>
2023-11-14 16:08:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ module "eks" {
   source                  = "./modules/eks"
   aws_public_subnet       = module.vpc.aws_public_subnet
   vpc_id                  = module.vpc.vpc_id
-  cluster_name            = "module-eks-${random_string.suffix.result}"
+  cluster_name            = "my_eks_cluster"
   endpoint_public_access  = true
   endpoint_private_access = false
   public_access_cidrs     = ["0.0.0.0/0"]
-  node_group_name         = "onlinetest"
+  node_group_name         = "eks_node_group"
   scaling_desired_size    = 1
   scaling_max_size        = 1
   scaling_min_size        = 1
