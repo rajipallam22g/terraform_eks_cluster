9beaccf74722854a473eee780a061070003135b4
Shaiknayeem753 <108930019+Shaiknayeem753@users.noreply.github.com>
2023-12-02 10:46:17 +0530

    Update main.tf
    update the instance type ,desired size,max_size,min_size

--- main.tf
+++ main.tf
@@ -8,10 +8,10 @@ module "eks" {
   endpoint_private_access = false
   public_access_cidrs     = ["0.0.0.0/0"]
   node_group_name         = "eks_node_group"
-  scaling_desired_size    = 1
-  scaling_max_size        = 1
-  scaling_min_size        = 1
-  instance_types          = ["t3.small"]
+  scaling_desired_size    = 2
+  scaling_max_size        = 2
+  scaling_min_size        = 2
+  instance_types          = ["t3a.medium"]
   key_pair                = "aws"
 }
 
