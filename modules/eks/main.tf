fdabff6b96d2e760ed599d9a7af38895e4bda4c9
Shaiknayeem753 <108930019+Shaiknayeem753@users.noreply.github.com>
2023-12-08 14:34:37 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -46,10 +46,16 @@ resource "aws_security_group" "node_group_one" {
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port = 80
-    to_port   = 80
+    from_port   = 22
+    to_port     = 22
     protocol  = "tcp"
 
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # This allows all traffic
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
