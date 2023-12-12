9bee825cde85de4634d0fdee3009cef56ffa7526
Shaiknayeem753 <108930019+Shaiknayeem753@users.noreply.github.com>
2023-12-12 13:48:33 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -52,6 +52,13 @@ resource "aws_security_group" "node_group_one" {
 
     cidr_blocks = ["0.0.0.0/0"]
   }
+ ingress {
+    from_port   = 30000
+    to_port     = 32767
+    protocol  = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   ingress {
     from_port   = 0
     to_port     = 0
