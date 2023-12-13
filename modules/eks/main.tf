498333c198fa51100eef1b5ff3d1a45982e28675
Shaiknayeem753 <108930019+Shaiknayeem753@users.noreply.github.com>
2023-12-13 12:58:35 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -138,3 +138,25 @@ resource "aws_iam_role_policy_attachment" "onlinetest-AmazonEKS_EBS_CSI_DriverRo
   role       = aws_iam_role.onlinetest2.name  # Replace with your IAM role name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
+
+resource "aws_iam_role" "ebs_csi_driver_role" {
+  name = "AmazonEKS_EBS_CSI_DriverRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "eks.amazonaws.com",
+        },
+      },
+    ],
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_driver_policy_attachment" {
+  role       = aws_iam_role.ebs_csi_driver_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
