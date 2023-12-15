4caf65510e0bb415c218dde546bd76b7b656607d
Shaiknayeem753 <108930019+Shaiknayeem753@users.noreply.github.com>
2023-12-15 15:44:08 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
     bucket         = "mybucketeks10"
     key            = "eks/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "ap-south-1"
     encrypt        = true
     dynamodb_table = "dynamo_db_table"
   }
