b321c4a25825dc790a4e292f9eebf566965655e2
rajipallam22g <113780580+rajipallam22g@users.noreply.github.com>
2023-12-25 16:48:44 +0530

    Update bucket.tf

--- s3_bucket/bucket.tf
+++ s3_bucket/bucket.tf
@@ -1,7 +1,7 @@
 
 # create s3 bucket
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "mybucketeks10" # change this
+  bucket = "myeksbucket3" # change this
 }
 
 resource "aws_dynamodb_table" "terraform_lock" {
