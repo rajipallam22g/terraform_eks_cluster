8ffc30ce7a74303807c03ca271e28b6f834c82eb
rajipallam22g <113780580+rajipallam22g@users.noreply.github.com>
2023-12-25 17:12:40 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
-/*
+
 terraform {
   backend "s3" {
-    bucket         = "myeksbucket3"
+    bucket         = "mybucket3"
     key            = "eks/terraform.tfstate"
     region         = "ap-southeast-2"
     encrypt        = true
     dynamodb_table = "dynamo_db_table"
   }
-*/
+
